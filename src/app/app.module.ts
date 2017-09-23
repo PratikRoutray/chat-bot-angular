c5d38c8765cc5d7b98f7b9eba9cadf913383accc
pratikroutray <elisa.pratik@gmail.com>
2017-09-23 16:19:16 +0530

    local

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppComponent }  from './app.component';
+import { app_routing } from './app.route.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, app_routing.routes,HttpModule ],
+  declarations: [ AppComponent, app_routing.components ],
+  providers:[],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
