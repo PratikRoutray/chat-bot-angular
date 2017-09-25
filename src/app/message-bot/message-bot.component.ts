1652b5c8ccf6c931172b31c2952b026734231fe9
arjun <elisa.pratik@gmail.com>
2017-09-26 00:10:40 +0530

    Added few validations

--- src/app/message-bot/message-bot.component.ts
+++ src/app/message-bot/message-bot.component.ts
@@ -32,7 +32,7 @@ export class MessageBotComponent implements OnInit {
   @ViewChild('chatHistory') chatHistory;
   constructor(private http: Http, private elementRef: ElementRef, private renderer: Renderer2) {
     let obj;
-    //this.getJSON().subscribe(data => obj=data, error => console.log(error));
+    // this.getJSON().subscribe(data => obj=data, error => console.log(error));
   }
   ngOnInit() {
     this.systemQuery = Query.Basic_Q1;
@@ -45,16 +45,43 @@ export class MessageBotComponent implements OnInit {
         case Query.Basic_Q1:
           this.name = this.userQuery;
           this.systemQuery = Query.Basic_Q2;
-          let chatSysQuery = 'Hello ' + this.name + ' ,' + Query.Basic_Q2;
+          const chatSysQuery = 'Hello ' + this.name + ' ,' + Query.Basic_Q2;
           this.createSystemQueryInChatHistory(chatSysQuery);
           break;
         case Query.Basic_Q2:
-          if (this.userQuery === UserQuery.Q1) {
-            //Need to implement all the scenario's here
+          if (this.userQuery.toUpperCase().indexOf('HOW MANY') > -1 || this.userQuery.toUpperCase().indexOf('COUNT') > -1) {
+            if (this.userQuery.toUpperCase().indexOf('ACCOUNT') > -1 &&
+              this.userQuery.toUpperCase().indexOf('ACTIVE') > -1) {
+              // FETCH Active account details
+              this.systemQuery = Query.Env_Q1;
+              this.checkEnvironmentDetails();
+            } else if (this.userQuery.toUpperCase().indexOf('ACCOUNT') > -1) {
+              // Fetch account specific information
+              this.systemQuery = Query.Env_Q1;
+              this.checkEnvironmentDetails();
+            } else if (this.userQuery.toUpperCase().indexOf('CLEARING INST') > -1) {
+              // Fetch clearing institute count
+              this.systemQuery = Query.Env_Q1;
+              this.checkEnvironmentDetails();
+            } else if (this.userQuery.toUpperCase().indexOf('CONTRA') > -1) {
+              // Fetch Contra count
+              this.systemQuery = Query.Env_Q1;
+              this.checkEnvironmentDetails();
+            }
 
           } else {
             this.systemQuery = Query.Resopnse_Ans;
             this.createSystemQueryInChatHistory(this.systemQuery);
+          }
+          break;
+        case Query.Env_Q1:
+          if (this.userQuery.toUpperCase() === 'DEV') {
+            // set env as DEV
+          } else if (this.userQuery.toUpperCase() === 'QA') {
+            // set env as QA
+          } else {
+            this.createSystemQueryInChatHistory('Please check ' + Query.Env_Q1 + '(DEV,QA)');
+            this.systemQuery = Query.Env_Q1;
           }
           break;
         default:
@@ -67,15 +94,20 @@ export class MessageBotComponent implements OnInit {
       this.userQuery = Constants.EMPTY_STRING;
     }
   }
+  checkEnvironmentDetails() {
+    this.systemQuery = Query.Env_Q1;
+    this.createSystemQueryInChatHistory(Query.Env_Q1);
+
+  }
   createSystemQueryInChatHistory(systemQuery) {
-    let chatElementSys = document.createElement('div');
+    const chatElementSys = document.createElement('div');
     chatElementSys.innerHTML = systemQuery;
     chatElementSys.innerText = systemQuery;
     chatElementSys.classList.add('rightAlign');
     this.chatHistory.nativeElement.appendChild(chatElementSys);
   }
   createUserQueryInChatHistory() {
-    let chatElement = document.createElement('div');
+    const chatElement = document.createElement('div');
     chatElement.innerHTML = this.userQuery;
     chatElement.innerText = this.userQuery;
     chatElement.classList.add('leftAlign');
